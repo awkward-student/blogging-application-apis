80ce98acb7083398190cb26782d1bc807f701ae2
Vishwas <im.vishwas.03@gmail.com>
2022-12-27 20:38:21 +0530

    Update CategoryController.java

--- src/main/java/com/app/blog/controllers/CategoryController.java
+++ src/main/java/com/app/blog/controllers/CategoryController.java
@@ -1,9 +1,12 @@
 package com.app.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -38,5 +41,17 @@ public class CategoryController {
 	public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Integer catId){
 		this.categoryService.deleteCategory(catId);
 		return new ResponseEntity<ApiResponse>(new ApiResponse("Category deleted successfully.", true), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{catId}")
+	public ResponseEntity<CategoryDTO> getCategory(@PathVariable Integer catId){
+		CategoryDTO categoryDto = this.categoryService.getCategory(catId);
+		return new ResponseEntity<CategoryDTO>(categoryDto, HttpStatus.OK);
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<List<CategoryDTO>> getCategories(){
+		List<CategoryDTO> categories = this.categoryService.getCategories();
+		return ResponseEntity.ok(categories);
 	}
 }
