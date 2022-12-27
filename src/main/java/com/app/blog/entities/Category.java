4211f3147ad43c72752ce261a4b395334b032a03
Vishwas <im.vishwas.03@gmail.com>
2022-12-28 00:07:53 +0530

    Updated Category.java and User.java
    Managed relationships among entities.
    Used Spring - JPA annotations and created Lists to store related data.

--- src/main/java/com/app/blog/entities/Category.java
+++ src/main/java/com/app/blog/entities/Category.java
@@ -1,10 +1,16 @@
 package com.app.blog.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,4 +29,7 @@ public class Category {
 	private String categoryTitle;
 	@Column(name = "description")
 	private String categoryDescription;
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	//here set can be used and may result more efficiently 
+	private List<Post> posts = new ArrayList<>();
 }
--- src/main/java/com/app/blog/entities/User.java
+++ src/main/java/com/app/blog/entities/User.java
@@ -1,10 +1,16 @@
 package com.app.blog.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,5 +31,7 @@ public class User {
 	private String email;
 	private String password;
 	private String about;
-	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	//here set can be used and may result more efficiently 
+	private List<Post> posts = new ArrayList<>();
 }
